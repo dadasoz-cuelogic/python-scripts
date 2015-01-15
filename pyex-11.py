349114e361f25f7009f6607d0aeff479b26cbd14
shruti <shrutih-cuelogic>
2015-01-15 17:40:07 +0530

    15-01-2015

---
+++ pyex-11.py
@@ -0,0 +1,12 @@
+print "How old are you?",
+age = raw_input()
+
+print "How tall are you?",
+height = raw_input()
+
+print "How much do you weigh?",
+weight = raw_input()
+
+print "\nSo, you're %s old\n, %r tall \n %r heavy." % (age, height, weight)
+
+
---
+++ pyex-16.py
@@ -0,0 +1,40 @@
+from sys import argv
+
+script, filename = argv
+
+
+print "We'r going to erase %r. " % filename
+print "If you don't want that, hit CTRL-C(^)."
+print "If you don't want that, hit RETURN."
+
+raw_input("?")
+
+print "Opening the file...."
+target = open(filename, 'w')
+
+print "Truncating the file.   Goodbye!"
+target.truncate()
+
+print "Now I'm going to ask you for three lines."
+
+line1= raw_input("line 1: ")
+line2= raw_input("line 1: ")
+line3= raw_input("line 1: ")
+
+print "I'm going to write these to the file."
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+target.write("\n")
+target.write("\n")
+
+target.write ('%s\n %r\n %r\n' % (line1, line2, line3))
+
+
+print "And finally, we close it. "
+target.close()
+
